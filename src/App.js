9c7bc6e8c8ae07bbaefb3859319e4e5d712eaeb1
Lucas Vieira <--replace-all>
2019-07-15 17:03:15 -0300

    add main project structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Page from './components/Page'
 
 function App() {
   return (
-    <div className="App">
-      <h2> Regex - the simple guide </h2>
-      <p> a simple guide for getting started with Regex </p>
+    <div className="main">
+      <div className="header">
+        <h1> Regex - the simple guide </h1>
+        <p> a simple guide for getting started with Regex </p>
+      </div>
+      <Page title='teste1' text='meu texto 1' classname='class1'></Page>
+      <Page title='teste2' text='meu texto 2' classname='class4'></Page>
     </div>
   );
 }
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
-const page = () => {
+const page = ({title, text, classname}) => {
 
+    return (
+        <div className={`page ${classname}`}>
+            <h2>{title}</h2>
+            <p>{text}</p>
+        </div>
+    )
 }
 
 export default page
