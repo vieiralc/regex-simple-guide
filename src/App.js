cf6d23bc62f6469c81d325db621cb2732f9d6380
Lucas Vieira <--replace-all>
2019-07-16 13:52:49 -0300

    add random class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react'
 import Page from './components/Page'
+import ins from './data/instructions.json'
 
 function App() {
+
+  const [texts] = useState(ins)
+
+  const getRandomNumber = () => {
+    let random = Math.floor(Math.random() * 5) + 1
+    return random
+  }
+
   return (
     <div className="main">
-      <div className="header">
+      <header className="header">
         <h1> Regex - the simple guide </h1>
         <p> a simple guide for getting started with Regex </p>
-      </div>
-      <Page title='teste1' text='meu texto 1' classname='class1'></Page>
-      <Page title='teste2' text='meu texto 2' classname='class4'></Page>
+      </header>
+
+      <section className="body">
+        {
+          texts.map((text, index) => 
+            <Page key={index} title={text.title} text={text.text} classname={`class${getRandomNumber()}`}/>
+          )
+        }
+      </section>
     </div>
   );
 }
