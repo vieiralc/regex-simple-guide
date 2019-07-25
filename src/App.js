97b71ceed96cf37df1d804293e6e2bde5e223a59
vieiralc <vieira.lucasmm@gmail.com>
2019-07-25 00:40:56 -0300

    change initial page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Notifications, { notify } from 'react-notify-toast'
 import Messages from './components/Messages'
+import meme from './img/meme.png'
 
 function App() {
 
@@ -22,10 +23,19 @@ function App() {
       </header>
 
       <section className="body">
-        <div id="splitpage" className={`page class1`}>
+        <div id="simplepage" className={`page class1`}>
+          <h3>Chapter 0: </h3>
+          <h3><i>Why Regex?</i></h3> 
+          <p className="text">Short for <b>regular expression</b>, it allows you to create patterns to match or locate any text.
+            It is very useful in many situation in computer programming. Regex can save you lots of time and effort. </p>
+          <img src={meme} alt="regex meme" width="300"/>  
+        </div>
+
+        <div id="splitpage" className={`page class2`}>
           <div className="left">
-              <h3>teste1</h3>
-              <p>texte1</p>
+              <h3>Chapter 1: </h3>
+              <h3><i>Character classes</i></h3> 
+              <p className="text"></p>
           </div>
           <div className="right">
             <h5>Example: [0-9]{"{1}"}\.[0-9]{"{2}"} </h5>
@@ -45,24 +55,100 @@ function App() {
           </div>
         </div>
 
-        <div id="simplepage" className={`page class2`}>
-            <h3>teste2</h3>
-            <p>teste2</p>
+        <div id="splitpage" className={`page class3`}>
+          <div className="left">
+              <h3>Chapter 2: </h3>
+              <h3><i>Anchors</i></h3> 
+              <p className="text"></p>
+          </div>
+          <div className="right">
+            <h5>Example: [0-9]{"{1}"}\.[0-9]{"{2}"} </h5>
+            <form onSubmit={handleSubmit}>
+              
+              <input 
+                type="text"
+                id="myInput"
+                className="form-control mb-2"
+                placeholder="Some text..." 
+                pattern="[0-9]{1}\.[0-9]{2}"
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+              />
+              <button id="myButton" type="submit">Test!</button>
+            </form>
+          </div>
         </div>
 
-        <div id="simplepage" className={`page class3`}>
-            <h3>teste2</h3>
-            <p>teste2</p>
+        <div id="splitpage" className={`page class4`}>
+          <div className="left">
+              <h3>Chapter 3: </h3>
+              <h3><i>Escaped Characters</i></h3> 
+              <p className="text"></p>
+          </div>
+          <div className="right">
+            <h5>Example: [0-9]{"{1}"}\.[0-9]{"{2}"} </h5>
+            <form onSubmit={handleSubmit}>
+              
+              <input 
+                type="text"
+                id="myInput"
+                className="form-control mb-2"
+                placeholder="Some text..." 
+                pattern="[0-9]{1}\.[0-9]{2}"
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+              />
+              <button id="myButton" type="submit">Test!</button>
+            </form>
+          </div>
         </div>
 
-        <div id="simplepage" className={`page class4`}>
-            <h3>teste2</h3>
-            <p>teste2</p>
+        <div id="splitpage" className={`page class5`}>
+          <div className="left">
+              <h3>Chapter 4: </h3>
+              <h3><i>Groups & Lookaround</i></h3> 
+              <p className="text"></p>
+          </div>
+          <div className="right">
+            <h5>Example: [0-9]{"{1}"}\.[0-9]{"{2}"} </h5>
+            <form onSubmit={handleSubmit}>
+              
+              <input 
+                type="text"
+                id="myInput"
+                className="form-control mb-2"
+                placeholder="Some text..." 
+                pattern="[0-9]{1}\.[0-9]{2}"
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+              />
+              <button id="myButton" type="submit">Test!</button>
+            </form>
+          </div>
         </div>
 
-        <div id="simplepage" className={`page class5`}>
-            <h3>teste2</h3>
-            <p>teste2</p>
+        <div id="splitpage" className={`page class5`}>
+          <div className="left">
+              <h3>Chapter 5: </h3>
+              <h3><i>Quantifiers & Alternations</i></h3> 
+              <p className="text"></p>
+          </div>
+          <div className="right">
+            <h5>Example: [0-9]{"{1}"}\.[0-9]{"{2}"} </h5>
+            <form onSubmit={handleSubmit}>
+              
+              <input 
+                type="text"
+                id="myInput"
+                className="form-control mb-2"
+                placeholder="Some text..." 
+                pattern="[0-9]{1}\.[0-9]{2}"
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+              />
+              <button id="myButton" type="submit">Test!</button>
+            </form>
+          </div>
         </div>
       </section>
     </div>
